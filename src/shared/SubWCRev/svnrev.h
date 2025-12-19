d0e15c26676dc2d10e45fe052321bef1af58f7dc
Pixelsuft <Pixelsuft@gmail.com>
2025-12-19 10:34:53 +0700

    fix_ver

--- src/shared/SubWCRev/svnrev.h
+++ src/shared/SubWCRev/svnrev.h
@@ -1,4 +1,4 @@
 // this should be overridden by the one generated in the "shared" directory,
 // but this fallback version is here just in case (to prevent compiler errors)
-#define SRCVERSION -1
-#define SRCVERSIONSTRING "-1"
+#define SRCVERSION 90
+#define SRCVERSIONSTRING "r90"
